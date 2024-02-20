db5bd90666fab2916e32e8f7202c92dbb0630007
rzoolu <3238290+rzoolu@users.noreply.github.com>
2024-02-20 16:06:38 +0100

    Add basic ServerApp UTs and needed mocks.

---
+++ server_src/tests/ServerAppTest.cpp
@@ -0,0 +1,188 @@
+
+#include <ServerApp.h>
+
+#include <AccessPointsDataI.h>
+#include <FileMonitorI.h>
+#include <JsonParserI.h>
+#include <MessagePublisherI.h>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using testing::_;
+using testing::Return;
+using testing::StartsWith;
+
+class MockAccessPointsData : public AccessPointsDataI
+{
+public:
+    MOCK_METHOD(const AccessPointMap_t&, getCurrentAPs, (), (const, override));
+    MOCK_METHOD(ChangeList_t, update, (const AccessPointMap_t&), (override));
+};
+
+class MockFileMonitor : public FileMonitorI
+{
+public:
+    MOCK_METHOD(void, startMonitoring, (), (override));
+};
+
+class MockJsonParser : public JsonParserI
+{
+public:
+    MOCK_METHOD(std::optional<AccessPointMap_t>, parseFromStream, (std::istream & stream), (override));
+    MOCK_METHOD(std::optional<AccessPointMap_t>, parseFromFile, (const std::filesystem::path&), (override));
+};
+
+class MockMessagePublisher : public MessagePublisherI
+{
+public:
+    MOCK_METHOD(void, sendToSubscribers, (const std::string&), (override));
+};
+
+namespace
+{
+constexpr auto DUMMY_JSON_PATH = "/dummy/path/ap.json";
+}
+
+class ServerAppTest : public testing::Test
+{
+protected:
+    ServerAppTest()
+    {
+        setupMocksInFactories();
+    }
+
+    void setupMocksInFactories()
+    {
+        setupMockAccessPointsDataFactory();
+        setupMockFileMonitorFactory();
+        setupMockJsonParserFactory();
+        setupMockMessagePublisherFactory();
+    }
+
+    void setupMockAccessPointsDataFactory()
+    {
+        AccessPointsDataI::create = [this]()
+        {
+            auto uptr = std::make_unique<MockAccessPointsData>();
+            m_mockAccessPointData = uptr.get();
+            return uptr;
+        };
+    }
+
+    void setupMockFileMonitorFactory()
+    {
+        FileMonitorI::create = [this]([[maybe_unused]] const std::filesystem::path& file,
+                                      [[maybe_unused]] FileObserverI& observer)
+        {
+            auto uptr = std::make_unique<MockFileMonitor>();
+            m_mockFileMonitor = uptr.get();
+            return uptr;
+        };
+    }
+
+    void setupMockJsonParserFactory()
+    {
+        JsonParserI::create = [this]()
+        {
+            auto uptr = std::make_unique<MockJsonParser>();
+            m_mockJsonParser = uptr.get();
+            return uptr;
+        };
+    }
+
+    void setupMockMessagePublisherFactory()
+    {
+        MessagePublisherI::create = [this]([[maybe_unused]] std::uint16_t tcpPort)
+        {
+            auto uptr = std::make_unique<MockMessagePublisher>();
+            m_mockMessagePublisher = uptr.get();
+            return uptr;
+        };
+    }
+
+protected:
+    MockAccessPointsData* m_mockAccessPointData = nullptr;
+    MockFileMonitor* m_mockFileMonitor = nullptr;
+    MockJsonParser* m_mockJsonParser = nullptr;
+    MockMessagePublisher* m_mockMessagePublisher = nullptr;
+};
+
+class ServerAppCreatedTest : public ServerAppTest
+{
+protected:
+    ServerAppCreatedTest() : m_serverApp(DUMMY_JSON_PATH)
+    {
+    }
+
+    ServerApp m_serverApp;
+};
+
+TEST_F(ServerAppTest, createDestroyGracefully)
+{
+    ServerApp app(DUMMY_JSON_PATH);
+}
+
+TEST_F(ServerAppCreatedTest, whenServerAppIsStartedFileMonitoringBegins)
+{
+    EXPECT_CALL(*m_mockFileMonitor, startMonitoring);
+
+    m_serverApp.run();
+}
+
+TEST_F(ServerAppCreatedTest, whenFileModificationIsReportedJsonIsParsed)
+{
+    EXPECT_CALL(*m_mockJsonParser, parseFromFile(_));
+
+    auto* serverAppAsFileObserver = static_cast<FileObserverI*>(&m_serverApp);
+
+    serverAppAsFileObserver->handleFileEvent(FileObserverI::FileModified);
+}
+
+TEST_F(ServerAppCreatedTest, whenNewJsonContentIsInvalidApDataIsNotUpdated)
+{
+    const std::optional<AccessPointMap_t> invalidParsingResult = std::nullopt;
+
+    EXPECT_CALL(*m_mockJsonParser, parseFromFile).WillOnce(Return(invalidParsingResult));
+
+    EXPECT_CALL(*m_mockAccessPointData, update(_)).Times(0);
+
+    auto* serverAppAsFileObserver = static_cast<FileObserverI*>(&m_serverApp);
+
+    serverAppAsFileObserver->handleFileEvent(FileObserverI::FileModified);
+}
+
+TEST_F(ServerAppCreatedTest, whenNewJsonContentIsValidApDataIsUpdatedWithIt)
+{
+    const AccessPoint validAp{"ssidx", 1, 1};
+    const auto validParsingResult =
+        std::make_optional<AccessPointMap_t>({{validAp.SSID, validAp}});
+
+    EXPECT_CALL(*m_mockJsonParser, parseFromFile).WillOnce(Return(validParsingResult));
+
+    EXPECT_CALL(*m_mockAccessPointData, update(*validParsingResult));
+
+    auto* serverAppAsFileObserver = static_cast<FileObserverI*>(&m_serverApp);
+
+    serverAppAsFileObserver->handleFileEvent(FileObserverI::FileModified);
+}
+
+TEST_F(ServerAppCreatedTest, whenAfterDataUpdateChangeIsDetectedMsgIsSent)
+{
+    const AccessPoint validAp{"ssidx", 1, 1};
+    const auto validParsingResult =
+        std::make_optional<AccessPointMap_t>({{validAp.SSID, validAp}});
+
+    EXPECT_CALL(*m_mockJsonParser, parseFromFile).WillOnce(Return(validParsingResult));
+
+    const APDataChange newApChange{APDataChange::NewAP, {}, validAp, {}};
+    const ChangeList_t changeList{newApChange};
+
+    EXPECT_CALL(*m_mockAccessPointData, update(*validParsingResult)).WillOnce(Return(changeList));
+
+    EXPECT_CALL(*m_mockMessagePublisher, sendToSubscribers(StartsWith("New AP"))).Times(1);
+
+    auto* serverAppAsFileObserver = static_cast<FileObserverI*>(&m_serverApp);
+
+    serverAppAsFileObserver->handleFileEvent(FileObserverI::FileModified);
+}
